1711e5875e119bca4165c937e4bffead1f83b834
Peter Stefek <peterstefek@macbook-pro.myfiosgateway.com>
2022-11-16 20:28:58 -0500

    .

--- deepthinking/utils/testing.py
+++ deepthinking/utils/testing.py
@@ -93,7 +93,7 @@ def test_default(net, testloader, iters, problem, device, test_type):
                         sampled_input = inputs[j,0].int()
                         sampled_pred = predicted[j].view(-1, *in_shape)
                         sampled_target = targets[j].view(-1, *in_shape)
-                        predicted_vid.append(sampled_pred)
+                        predicted_vid.append(sampled_pred.cpu().numpy())
 
                         percentage_correct_bits = (sampled_pred == sampled_target).sum() / reduce(operator.mul, in_shape, 1) * 100
                         
