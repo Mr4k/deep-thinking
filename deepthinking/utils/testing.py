2893a4083af0c10d84f1b7ea0bd96a24d01bbc66
Peter Stefek <peterstefek@macbook-pro.myfiosgateway.com>
2022-11-16 21:10:48 -0500

    .

--- deepthinking/utils/testing.py
+++ deepthinking/utils/testing.py
@@ -88,7 +88,7 @@ def test_default(net, testloader, iters, problem, device, test_type):
                 for j in range(num_data_pieces_to_report):
                     predicted_vid = []
                     for i in range(all_outputs.size(1)):
-                        outputs = all_outputs[:, i]
+                        outputs = all_outputs[:, i][0]
                         print("zzzz:", outputs.shape)
                         predicted = get_predicted(inputs, outputs, problem)
                         in_shape = inputs[j].shape[1:]
